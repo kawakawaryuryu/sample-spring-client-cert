e5f4923e455137b7faa94ef115813422446e7489
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2020-12-15 00:54:32 +0900

    initial commit

---
+++ src/main/java/com/kawakawaryuryu/samplespringclientcert/SampleSpringClientCertApplication.java
@@ -0,0 +1,13 @@
+package com.kawakawaryuryu.samplespringclientcert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringClientCertApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SampleSpringClientCertApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/kawakawaryuryu/samplespringclientcert/SampleSpringClientCertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kawakawaryuryu.samplespringclientcert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringClientCertApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
